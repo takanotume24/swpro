275fc23b4de9a89070573133e56964e225c74cec
takanotume24 <takanotume24@gmail.com>
2019-12-14 16:57:20 +0000

    一通り実装できた

--- src/switch-proxy.cr
+++ src/switch-proxy.cr
@@ -5,6 +5,17 @@ module Switch::Proxy
   VERSION = "0.1.0"
 
   class MyCli < Clim
+    PATH_CONF_APT   = "./apt.conf"
+    HTTP_PROXY_APT  = "Acquire::http::Proxy"
+    HTTPS_PROXY_APT = "Acquire::https::Proxy"
+    CONFIG_APT      = [HTTP_PROXY_APT, HTTPS_PROXY_APT]
+
+    # def initialize
+    #   @@http_proxy  = "Acquire::http::Proxy"
+    #   @@https_proxy = "Acquire::https::Proxy"
+    #   @@configs = [@@http_proxy, @@https_proxy]
+    # end
+
     main do
       desc "proxy setting tool."
       usage "swpro [sub_command] [arguments]"
@@ -18,7 +29,26 @@ module Switch::Proxy
         usage "swpro enable [command]"
 
         run do |opts, args|
-          puts "proxy enabled (^_^)"
+          if (args.size < 1)
+            puts opts.help_string
+            exit()
+          end
+
+          _command = args[0]
+
+          case _command
+          when "apt"
+            path = PATH_CONF_APT
+            content = File.read path
+            content = content.gsub /.*#.*#{HTTP_PROXY_APT}/, "#{HTTP_PROXY_APT}"
+            content = content.gsub /.*#.*#{HTTPS_PROXY_APT}/, "#{HTTPS_PROXY_APT}"
+
+            File.write(path, content)
+            puts "#{_command}のプロキシ設定を有効化しました｡ (^_^)"
+          else
+            puts "このコマンドは対応していません (>_<)"
+            exit()
+          end
         end
       end
 
@@ -36,11 +66,13 @@ module Switch::Proxy
 
           case _command
           when "apt"
-            path = "/etc/apt/apt.conf"
+            path = PATH_CONF_APT
             content = File.read path
+            content = content.gsub /.*#{HTTP_PROXY_APT}/, "# #{HTTP_PROXY_APT}"
+            content = content.gsub /.*#{HTTPS_PROXY_APT}/, "# #{HTTPS_PROXY_APT}"
 
-            content.gsub "Acquire::http::Proxy", "# Acquire::http::Proxy"
-            content.gsub "Acquire::https::Proxy", "# Acquire::https::Proxy"
+            File.write(path, content)
+            puts "#{_command}のプロキシ設定を無効化しました｡(^_^)"
           else
             puts "このコマンドは対応していません (>_<)"
             exit()
@@ -63,7 +95,7 @@ module Switch::Proxy
 
           case _command
           when "apt"
-            path = "./apt.conf"
+            path = PATH_CONF_APT
 
             if (!File.writable? path)
               puts "#{path}に書き込みできません (>_<) 権限を確認してください"
@@ -77,35 +109,33 @@ module Switch::Proxy
             end
 
             content = File.read path
-            http_proxy = "Acquire::http::Proxy"
-            https_proxy = "Acquire::https::Proxy"
-            configs = [http_proxy, https_proxy]
+            # http_proxy = "Acquire::http::Proxy"
+            # https_proxy = "Acquire::https::Proxy"
+            # configs = [http_proxy, https_proxy]
 
-            configs.each do |config|
+            CONFIG_APT.each do |config|
               regex = /.*#{config}.*/
               match_data = content.scan(regex)
-              
+
               if match_data.size == 0
                 new_line = "#{config} #{_url};\n"
                 content += new_line
                 puts "追加しました｡: #{new_line}"
                 next
               end
-              
+
               if match_data.size > 0
-                p match_data
-                printf "既に#{config}が存在します 書き換えますか(y/n)?"
-                
+                printf "既に#{match_data}が存在します 書き換えますか(y/n)?"
+
                 if read_line != "y"
                   puts "書き換えませんでした｡"
                   exit()
                 end
-                
+
                 content = content.gsub(regex, "#{config} #{_url};")
                 puts "書き換えました｡"
               end
             end
-            
           else
             puts "このコマンドは対応していません (>_<)"
             exit()
