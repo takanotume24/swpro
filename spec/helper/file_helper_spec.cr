0ef27262cabd7032a3c066842cf44db029f074a6
takanotume24 <takanotume24@gmail.com>
2020-08-09 16:00:53 +0900

    add spec FileHelper#check_writable

--- spec/helper/file_helper_spec.cr
+++ spec/helper/file_helper_spec.cr
@@ -1,11 +1,18 @@
 require "../../src/helper/file_helper"
 include Switch::Proxy::Helper::FileHelper
+include Switch::Proxy::Config
 
 describe Switch::Proxy::Helper::FileHelper do
-  it "check_writable" do
-    io = IO::Memory.new
-    path = Path.new("/usr/bin/apt")
-    check_writable path, io
-    io.to_s.includes?("[ERROR]").should be_true
+  describe "#check_writable" do
+    it "A file that shouldn't be able to be written." do
+      io = IO::Memory.new
+      path = Path.new("/usr/bin/apt")
+      check_writable(path, io).should be_nil
+    end
+    it "A file that could be written to." do 
+      io = IO::Memory.new
+      path = Path.new(UserConfig.get_path)
+      check_writable(path, io).should be_nil
+    end
   end
 end
