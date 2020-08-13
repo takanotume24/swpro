ae378dbb744986a6a8cccfb9c0464014c9bb2840
takanotume24 <takanotume24@gmail.com>
2020-08-13 22:36:17 +0900

    support relative path in install command.

--- src/commands/internal/cp_json.cr
+++ src/commands/internal/cp_json.cr
@@ -8,7 +8,15 @@ module Switch::Proxy::Commands::Internal
     files = ["proxy_list.json", "user_config.json"]
 
     files.each do |file|
-      src = Path["./configs/#{file}"].normalize.expand(home: true)
+      process_path = Process.executable_path
+      if process_path.nil?
+        io.puts error "Cannot get Process.executable_path."
+        return nil
+      end
+
+      process_path = Path[process_path]
+      io.puts process_path
+      src = Path[process_path.parent, "..","configs", file].normalize.expand(home: true)
       dest = Path["~/.swpro/#{file}"].normalize.expand(home: true)
       mkdir_p dest.parent
 
--- src/commands/internal/symlink.cr
+++ src/commands/internal/symlink.cr
@@ -3,14 +3,22 @@ module Switch::Proxy::Commands::Internal
   include Switch::Proxy::Helper::Common
 
   def symlink(opts, args, io)
-    new_path = Path["/bin/swpro"].normalize.expand(home: true).to_s
-    old_path = Path["./bin/swpro"].normalize.expand(home: true).to_s
+    process_path = Process.executable_path
+    if process_path.nil?
+      io.puts error "Cannot get Process.executable_path."
+      return nil
+    end
+
+    process_path = Path[process_path]
+
+    new_path = Path["/", "bin", "swpro"].normalize.expand(home: true).to_s
+    old_path = process_path.normalize.expand(home: true).to_s
     if File.file? new_path
       io.puts info "Symbolic link already exists."
       return -1
     end
     if !File.file? old_path
-      io.puts "[ERROR]\t #{old_path} does not exist."
+      io.puts error "#{old_path} does not exist."
       return -1
     end
     File.symlink old_path, new_path
